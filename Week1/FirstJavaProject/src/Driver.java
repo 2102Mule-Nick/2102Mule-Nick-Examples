c704fd3105b306bda0d19adbf88c4124621a26ae
jurczakn <nickolas.jurczak@gmail.com>
2021-02-22 15:22:59 -0500

    hello world

---
+++ Week1/FirstJavaProject/src/Driver.java
@@ -0,0 +1,15 @@
+
+public class Driver {
+	
+	//V--- access modifier, public is available everywhere in the application
+	//     V---- don't need to instantiate object to use (belongs to the class)
+	//            V---- doesn't return anything
+	//                 V----- method name (must be named 'main')
+	//                      V---- parameter (can take any number of string input from the command line)
+	static public void main(String[] pirateSounds) {
+		//our main method
+		//entry point to application
+		System.out.println("Hello world");
+	}
+	
+}
