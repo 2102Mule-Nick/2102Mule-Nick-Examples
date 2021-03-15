59bf8b58423a41d5eb91191bf1d27b080fa3710a
jurczakn <nickolas.jurczak@gmail.com>
2021-03-15 18:09:35 -0400

    JMS notes and Spring JMS config

--- Week3/POSAutowired/src/main/java/com/revature/Driver.java
+++ Week3/POSAutowired/src/main/java/com/revature/Driver.java
@@ -1,5 +1,7 @@
 package com.revature;
 
+import org.apache.activemq.broker.BrokerFactory;
+import org.apache.activemq.broker.BrokerService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -19,6 +21,21 @@ public class Driver {
 			menu.displayOptions();
 			menu = menu.advance();
 		} while (menu != null);
+		
+	}
+	
+	private static void setUpActiveMQ() {
+		
+		BrokerService broker;
+		
+		try {
+			broker = BrokerFactory.createBroker("broker:(" + AppConfig.BROKER_URL + ")");
+			broker.getAdminView();
+			broker.start();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
---
+++ Week3/POSAutowired/src/main/java/com/revature/messaging/JmsMessageListener.java
@@ -0,0 +1,14 @@
+package com.revature.messaging;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+public class JmsMessageListener implements MessageListener {
+
+	@Override
+	public void onMessage(Message message) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
